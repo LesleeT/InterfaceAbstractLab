3f796b81eed7047cd325d2b2f4e819740356ffdb
ltrinastic <ltrinastic@L11721043664.wctc.msft>
2017-02-15 15:46:11 -0600

    test

---
+++ src/lab1/IntroJavaCourse.java
@@ -0,0 +1,28 @@
+package lab1;
+import javax.swing.JOptionPane;
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroJavaCourse extends ProgrammingDegreeCourses{
+    //private String courseName;
+    //private String courseNumber;
+    private double credits;
+
+    public IntroJavaCourse(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+
+    @Override
+    public void setCredits(double credits) {
+       if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+}
---
+++ src/lab1/Startup.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ltrinastic
+ */
+public class Startup {
+    public static void main(String[] args) {
+        IntroJavaCourse introJava = new IntroJavaCourse("Intro to Java","222");
+        AdvancedJavaCourse advancedJava = new AdvancedJavaCourse("Advanced Java","444");
+        IntroToProgrammingCourse introProg = new IntroToProgrammingCourse("Intro to Programming", "111");
+
+        
+        College college = new College();
+        
+    }
+    
+}
